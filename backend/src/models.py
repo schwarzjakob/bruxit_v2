4ca8f5697e2f5fe3e93d2aa12b380747745e0e1b
epura <eleonora_pura@hotmail.it>
2024-09-09 16:32:29 +0200

    updated db models

--- backend/src/models.py
+++ backend/src/models.py
@@ -12,6 +12,7 @@ class Patient(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
     
+"""    
 class PatientData(db.Model):
     __tablename__ = 'patient_data'
 
@@ -28,6 +29,36 @@ class PatientData(db.Model):
 
     def __repr__(self):
         return f'<Patient {self.patient_id}>: <Night {self.night_id}>, <Week {self.week}>, <Recorder {self.recorder}>\n, <Data Point {self.data_point}>, <MR {self.mr}>, <ML {self.ml}>, <ECG {self.ecg}>'
+"""    
+
+
+class NightDuration(db.Model):
+    __tablename__ = 'night_duration'
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, nullable=False)
+    week = db.Column(db.String(50), nullable=False)
+    file = db.Column(db.String(50), unique=True, nullable=False)
+    seconds = db.Column(db.Float, nullable=False)
+    duration = db.Column(db.Interval, nullable = False)
+
+    def __repr__(self):
+        return f'<Patient {self.patient_id}>: week {self.week}, file: {self.file}, seconds: {self.seconds}, duration: {self.duration}'
+
+
+class SSD(db.Model):
+    __tablename__ = 'ssd'
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, nullable=False)
+    week = db.Column(db.String(50), nullable=False)
+    file = db.Column(db.String(50), nullable=False)
+    x = db.Column(db.Integer, nullable=False)
+    y = db.Column(db.Integer, nullable=False)
+    HRV_LFHF = db.Column(db.Float, nullable=False)
+    HRV_SDNN = db.Column(db.Float, nullable=False)
+    stage = db.Column(db.String(50), nullable=False)
+    selected = db.Column(db.Boolean, unique=False, default=False)
+
+
 
 
     
